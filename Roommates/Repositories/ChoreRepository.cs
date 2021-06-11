2b04a55ae20a792ac00be660d5c338081a71525a
bvinson23 <brandonvinson23@gmail.com>
2021-06-11 15:08:06 -0500

    first commit

---
+++ Roommates/Repositories/ChoreRepository.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roommates.Repositories
+{
+    /// <summary>
+    ///  This class is responsible for interacting with Chore data.
+    ///  It inherits from the BaseRepository class so that it can use the BaseRepository's Connection property
+    /// </summary>
+    public class ChoreRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new ChoreRepository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        /// <summary>
+        /// Get a list of all Chores in the database
+        /// </summary>
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name from Chore";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Chore> chores = new List<Chore>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        Chore chore = new Chore
+                        {
+                            Id = idValue,
+                            Name = nameValue,
+                        };
+
+                        chores.Add(chore);
+                    }
+
+                    reader.Close();
+
+                    return chores;
+                }
+            }
+        }
+
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name from Chore where Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Chore chore = null;
+
+                    if (reader.Read())
+                    {
+                        chore = new Chore
+                        {
+                            Id = id,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+                    }
+
+                    reader.Close();
+
+                    return chore;
+                }
+            }
+        }
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
+    }
+}
---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Concurrent;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name"
+                }
+            }
+        }
+    }
+}
